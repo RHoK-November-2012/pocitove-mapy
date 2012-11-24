5e4991e2d55dcec33ba92aac967d2adf02b30c30
Lukas Lansky <lansky@elitecode.cz>
2012-11-24 14:03:14 +0100

    Some effort on adding shapes.

--- routes/maps.js
+++ routes/maps.js
@@ -1,9 +1,18 @@
-
-/*
- * GET home page.
- */
+var shred = require("shred");
 
 exports.add_routes = function(req, res){
-    // TODO ...
-    res.render('index', { title: 'Express', user: req.session['user'] });
+	shred.get({
+		url: "https://www.googleapis.com/fusiontables/v1/tables/17H2KNPhUpX244CDr6RFSzpJFvRG_zN-CpzkPYwY/columns?key=AIzaSyBmIHT_b4PoGIXOztnFkbU6KDgPIdeFqwE",
+		headers: {
+			Accept: "application/json"
+		},
+		on: {
+			200: function (columns) {
+				console.log(columns.content.data);
+			}
+		},
+		response: function (response) {
+			console.log("Fuck.");
+		}
+	});
 };
