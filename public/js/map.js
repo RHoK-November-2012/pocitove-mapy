188f08c03452d005c57fc6edb842097365496ffe
Lukas Lansky <lansky@elitecode.cz>
2012-11-25 14:29:52 +0100

    ...

--- public/js/map.js
+++ public/js/map.js
@@ -10,151 +10,22 @@
       streetViewControl: false
       ,
       styles: [
-        {
-          featureType: "landscape.natural",
-          stylers: [
-            { color: "#ffffff" }
-          ]
-        },{
-          featureType: "administrative",
-          elementType: "geometry",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          featureType: "administrative",
-          elementType: "labels.text",
-          stylers: [
-            { visibility: "off" },
-          ]
-        },{
-          featureType: "administrative.country",
-          elementType: "geometry",
-          stylers: [
-            { weight: 1 },
-            { visibility: "on" },
-            { color: "#010100" }
-          ]
-        },{
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          featureType: "road",
-          elementType: "geometry.fill",
-          stylers: [
-            { weight: 0.7 },
-            { color: "#006464" }
-          ]
-        },{
-          featureType: "road",
-          elementType: "labels.icon",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          elementType: "labels.text",
-          stylers: [
-            { visibility: "off" },
-            { color: "#ffffff" }
-          ]
-        },{
-          featureType: "landscape",
-          elementType: "labels.text",
-          stylers: [
-            { visibility: "off" },
-            { color: "#007575" }
-          ]
-        },{
-          featureType: "poi",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          featureType: "administrative.land_parcel",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          featureType: "poi.park",
-          stylers: [
-            { visibility: "on" }
-          ]
-        },{
-          featureType: "poi.park",
-          elementType: "labels.icon",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          featureType: "transit",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          featureType: "water",
-          elementType: "geometry",
-          stylers: [
-            { color: "#007575" }
-          ]
-        },{
-          featureType: "poi.park",
-          elementType: "geometry.fill",
-          stylers: [
-            { color: "#eeeeee" }
-          ]
-        },
-        {
-          featureType: "poi.park",
-          elementType: "labels.text",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [
-            { visibility: "off" },
-            { color: "#ffffff" },
-            { weight: 3 }
-          ]
-        },{
-          featureType: "road",
-          elementType: "labels.text",
-          stylers: [
-            { visibility: "off" },
-            { color: "#ffffff" },
-            { weight: 3 }
-          ]
-        },{
-          featureType: "transit",
-          stylers: [
-            { weight: 0.5 },
-            { color: "#007575" }
-          ]
-        },{
-          featureType: "road",
-          elementType: "labels.icon",
-          stylers: [
-            { visibility: "off" }
-          ]
-        },{
-          featureType: "road.highway",
-          elementType: "geometry.fill",
-          stylers: [
-            { weight: 2 }
-          ]
-        },{
-          featureType: "road.arterial",
-          elementType: "geometry.fill",
-          stylers: [
-            { weight: 1.3 }
-          ]
-        }
-      ]
+  {
+    "elementType": "labels",
+    "stylers": [
+      { "visibility": "off" }
+    ]
+  },{
+    "stylers": [
+      { "saturation": -75 }
+    ]
+  },{
+    "featureType": "road",
+    "stylers": [
+      { "weight": 1 }
+    ]
+  }
+]
     };
     map = new google.maps.Map(document.getElementById("map"), myOptions);
 
